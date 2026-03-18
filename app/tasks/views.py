f601cec0bc5802874b8507c782486b5d50f3e7db
SamChavez <sa5m.escom@gmail.com>
2026-03-17 21:09:22 -0600

    feat: task EP's,  and add swagger

--- app/tasks/views.py
+++ app/tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        # Automatically set created_by/updated_by to the current user
+        # For this demo, we can set them to the request user
+        # (Assuming authentication is set up, otherwise we might need a dummy)
+        serializer.save(created_by=self.request.user, updated_by=self.request.user)
