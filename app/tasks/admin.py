db388c473a80e5f8bfc28fef128b0e9aeaf986b8
SamChavez <sa5m.escom@gmail.com>
2026-03-17 20:34:44 -0600

    feat: task model

--- app/tasks/admin.py
+++ app/tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_completed', 'created_at', 'updated_at')
+    list_filter = ('is_completed', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    ordering = ('-created_at',)
